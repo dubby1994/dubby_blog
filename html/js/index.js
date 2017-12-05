b3e4a244a7618516aa0d7fdb382b967f6f05439a
dubby <yangzheng03@meituan.com>
2017-12-05 18:20:57 +0800

    init

--- html/js/index.js
+++ html/js/index.js
@@ -1,3 +1,25 @@
 /**
  * Created by teeyoung on 17/12/5.
  */
+/**
+ * 查询最新10篇文章
+ */
+function freshTop10Blog() {
+    $.ajax({
+        type: 'get',
+        url: "blog/list",
+        cache: false,
+        dataType: 'json',
+        success: function (data) {
+            jQuery.each(data, function (i, item) {
+                var date = new Date();
+                date.setTime(item.createTime);
+
+                $("#blogList").append("");
+            });
+        },
+        error: function () {
+            return;
+        }
+    });
+}
